0b80d1c287159dd5adafa48d655593e3b082e3db
Gabor Szabo <szabgab@gmail.com>
2013-05-29 15:41:25 +0300

    separate fetch and process

--- bin/fetch.pl
+++ bin/fetch.pl
@@ -5,16 +5,43 @@ use 5.010;
 
 use WWW::Mechanize;
 use DateTime;
+use Data::ICal;
+use Data::Dumper qw(Dumper);
 
-my $m = WWW::Mechanize->new;
+use Getopt::Long qw(GetOptions);
+
+my%opt;
+GetOptions(\%opt,
+	'fetch',
+	'process',
+);
 
 # based on input
 my $url = "http://www.yapcna.org/yn2013";
 my $day = DateTime->new(year => 2013, month => 6, day => 3);
 my $length = 3;
 
-$m->get("$url/timetable.ics");
-say $m->content;
+mkdir 'tmp';
+my $ics = 'tmp/timetable.ics';
+
+if ($opt{fetch}) {
+	my $m = WWW::Mechanize->new;
+	$m->get("$url/timetable.ics");
+	open my $fh, '>:encoding(utf-8)', $ics;
+	print $fh $m->content;
+	close $fh;
+	exit;
+}
+if ($opt{process}) {
+	die "Missing $ics,  maybe --fetch first?\n" if not -e $ics;
+	my $calendar = Data::ICal->new( filename => $ics );
+	print $calendar;
+
+	foreach my $e (@{ $calendar->entries }) {
+		my $p = $e->properties;
+		print Dumper $p;
+		exit;
+	}
 
 #for (1..$length) {
 #	#say $day->ymd;
@@ -22,4 +49,8 @@ say $m->content;
 #
 #	$day->add_duration( DateTime::Duration->new(days => 1) );
 #}
+	exit;
+}
+
+die "Usage: $0 [--fetch|--process]\n";
 
